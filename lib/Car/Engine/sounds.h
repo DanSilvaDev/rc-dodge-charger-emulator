7fbaa124805f5c197d23dbe6a5be26eed2b2c769
daniel <daniel.felix05@gmail.com>
2025-11-10 11:12:23 -0300

    first commit

---
+++ lib/Car/Engine/sounds.h
@@ -0,0 +1,26 @@
+﻿//
+// Created by rvezy on 11/7/2025.
+//
+
+#ifndef RCDODGECHARGEREMULATION_SOUNDS_H
+#define RCDODGECHARGEREMULATION_SOUNDS_H
+#include <string>
+
+class EngineSounds {
+public:
+    EngineSounds();
+
+    std::string getIdle();
+    std::string getFuelPumpStart();
+    std::string getEngineStartSuccess();
+    std::string getEngineStartFailure();
+    std::string getEngineStop();
+    std::string getRevEngAtAccelerating(int rpm);
+    std::string getRevEngAtDecelerating(int rpm);
+    std::string getNeutralRevUp(int rpm);
+    std::string getNeutralRevDown(int rpm);
+
+};
+
+
+#endif //RCDODGECHARGEREMULATION_SOUNDS_H
